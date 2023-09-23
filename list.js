f34b678cdc4205c68831333ac4b2af9a9573a165
ShahbazAhmed786 <141067134+ShahbazAhmed786@users.noreply.github.com>
2023-09-24 00:23:24 +0500

    Add files via upload

--- list.js
+++ list.js
@@ -1,7 +1,14 @@
-// Store a list of people to invite to dinner
-var dinnerGuests = ['Ali Ahmed', 'Sheraz Ahmed', 'Shahbaz Khan'];
+var dinnerGuests = ['Ali Ahmed', 'Shahbaz khan', 'Sheraz Ahmed'];
 // Print an invitation message to each person
 for (var i = 0; i < dinnerGuests.length; i++) {
     var guest = dinnerGuests[i];
     console.log("Dear ".concat(guest, ", you are invited to dinner. It would be an honor to have you join us."));
 }
+// Assume one of the guests can't make it
+var unableToAttend = dinnerGuests[1];
+console.log("Unfortunately, ".concat(unableToAttend, " cannot make it to dinner."));
+// Send a new invitation to someone else
+var newGuest = 'Shehryar';
+dinnerGuests[1] = newGuest;
+// Print a new invitation message to the replacement guest
+console.log("Dear ".concat(newGuest, ", you are invited to dinner. We would love to have you join us as ").concat(dinnerGuests[1], " cannot attend."));
--- list.ts
+++ list.ts
@@ -1,8 +1,18 @@
-// Store a list of people to invite to dinner
-const dinnerGuests: string[] = ['Ali Ahmed', 'Sheraz Ahmed', 'Shahbaz Khan'];
+let dinnerGuests: string[] = ['Ali Ahmed', 'Shahbaz khan', 'Sheraz Ahmed'];
 
 // Print an invitation message to each person
 for (let i = 0; i < dinnerGuests.length; i++) {
   const guest: string = dinnerGuests[i];
   console.log(`Dear ${guest}, you are invited to dinner. It would be an honor to have you join us.`);
 }
+
+// Assume one of the guests can't make it
+const unableToAttend: string = dinnerGuests[1];
+console.log(`Unfortunately, ${unableToAttend} cannot make it to dinner.`);
+
+// Send a new invitation to someone else
+const newGuest: string = 'Shehryar';
+dinnerGuests[1] = newGuest;
+
+// Print a new invitation message to the replacement guest
+console.log(`Dear ${newGuest}, you are invited to dinner. We would love to have you join us as ${dinnerGuests[1]} cannot attend.`);

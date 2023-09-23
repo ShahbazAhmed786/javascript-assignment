f1cb503bd657e2aafa2cfabdb15b6df8646784a2
ShahbazAhmed786 <141067134+ShahbazAhmed786@users.noreply.github.com>
2023-09-24 00:15:16 +0500

    Add files via upload

---
+++ famper.js
@@ -0,0 +1,7 @@
+function Message(message) {
+    console.log(message);
+}
+var famousPerson = "Allma Muhammad Iqbal";
+var quote = "The ultimate aim of the ego is not to see something, but to be something.";
+var message = "".concat(famousPerson, " once said, \"").concat(quote, "\"");
+Message(message);
---
+++ famper.ts
@@ -0,0 +1,13 @@
+function Message(message: string): void {
+  console.log(message);
+}
+
+const famousPerson: string = "Allma Muhammad Iqbal";
+const quote: string = "The ultimate aim of the ego is not to see something, but to be something.";
+
+const message: string = `${famousPerson} once said, "${quote}"`;
+Message(message);
+
+
+
+

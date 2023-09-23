bd82220fbe4c83473c7afabdd8a56ff0e7216a2f
ShahbazAhmed786 <141067134+ShahbazAhmed786@users.noreply.github.com>
2023-09-24 00:17:06 +0500

    Add files via upload

---
+++ dmas.js
@@ -0,0 +1,11 @@
+function printResult(operation, result) {
+    console.log("".concat(operation, " = ").concat(result));
+}
+var additionResult = 5 + 3;
+printResult("5 + 3", additionResult);
+var subtractionResult = 10 - 2;
+printResult("10 - 2", subtractionResult);
+var multiplicationResult = 4 * 2;
+printResult("4 * 2", multiplicationResult);
+var divisionResult = 16 / 2;
+printResult("16 / 2", divisionResult);
---
+++ dmas.ts
@@ -0,0 +1,21 @@
+function printResult(operation: string, result: number): void {
+  console.log(`${operation} = ${result}`);
+}
+
+const additionResult: number = 5 + 3;
+printResult("5 + 3", additionResult);
+
+const subtractionResult: number = 10 - 2;
+printResult("10 - 2", subtractionResult);
+
+const multiplicationResult: number = 4 * 2;
+printResult("4 * 2", multiplicationResult);
+
+const divisionResult: number = 16 / 2;
+printResult("16 / 2", divisionResult);
+
+
+
+
+
+

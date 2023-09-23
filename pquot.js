b6a5e86aeec0ffff605c710f5daa7fb82f45e7f9
ShahbazAhmed786 <141067134+ShahbazAhmed786@users.noreply.github.com>
2023-09-24 00:14:28 +0500

    Add files via upload

---
+++ pquot.js
@@ -0,0 +1,6 @@
+function QuoteAndAuthor(quote, author) {
+    console.log("".concat(author, " once said, \"").concat(quote, "\""));
+}
+var quote = "The ultimate aim of the ego is not to see something, but to be something.";
+var author = "Allma Muhammad Iqbal";
+QuoteAndAuthor(quote, author);
---
+++ pquot.ts
@@ -0,0 +1,7 @@
+function QuoteAndAuthor(quote: string, author: string): void {
+  console.log(`${author} once said, "${quote}"`);
+}
+
+const quote: string = "The ultimate aim of the ego is not to see something, but to be something.";
+const author: string = "Allma Muhammad Iqbal";
+QuoteAndAuthor(quote, author);

e0b129a6534a91c359b9343901557c716043fe3e
ShahbazAhmed786 <141067134+ShahbazAhmed786@users.noreply.github.com>
2023-09-24 00:19:27 +0500

    Add files via upload

--- fnam.ts
+++ fnam.ts
@@ -1,4 +1,6 @@
-
+//Shahbaz Ahmed
+//Date: 01-8-23
 const myFavNumber: number = 7;
 const message: string = `My favorite number is ${myFavNumber}.`;
 console.log(message);
+//This program is printing your favorite number
